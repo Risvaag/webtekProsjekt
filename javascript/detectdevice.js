0bd917fea1002603608a9f1d59676678c5152969
Andreas Williams <andrewil@stud.ntnu.no>
2015-11-15 17:20:43 +0100

    hallo

---
+++ javascript/detectdevice.js
@@ -0,0 +1,8 @@
+addEventListener("load", function() {
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+ 	alert("Hello! This is a pre-written message from Sup Er K. Ewl. I have noticed that you are on a mobile device. We do not have a mobile friendly website up and running. We will redirect you to our mobile unfriendly site.")
+}
+	
+});
+
+
