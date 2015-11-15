d8dc2553e85df58fb05c9067d089292413c49e9a
Thomas Skuggevik <thomsku@stud.ntnu.no>
2015-11-15 13:46:54 +0100

    fjernet products' undersider i navbar

--- javascript/dropdown.js
+++ javascript/dropdown.js
@@ -3,16 +3,13 @@ addEventListener("load", function(){
 	document.getElementById("sup_dropfrom").addEventListener("mouseover", function(){ showMenu(1); });
 	document.getElementById("sup_dropfrom").addEventListener("mouseout", function(){ hideMenu(1); });
 
-	document.getElementById("pro_dropfrom").addEventListener("mouseover", function(){ showMenu(2); });
-	document.getElementById("pro_dropfrom").addEventListener("mouseout", function(){ hideMenu(2); });
-
-	document.getElementById("about_dropfrom").addEventListener("mouseover", function(){ showMenu(3); });
-	document.getElementById("about_dropfrom").addEventListener("mouseout", function(){ hideMenu(3); });
+	document.getElementById("about_dropfrom").addEventListener("mouseover", function(){ showMenu(2); });
+	document.getElementById("about_dropfrom").addEventListener("mouseout", function(){ hideMenu(2); });
 });
 
 function setup()
 {
-	var navbar = '<nav class="fixed-nav-bar"> <div id="menu" class="menu"><ul class="menu_items"><li id="logo"><a href="homepage.html"><img src="Images/logo.jpg" alt="logo"></a></li><li><a href="homepage.html">Home</a></li><li id="pro_dropfrom"><a href="products.html">Products&#9660;</a><ul id="pro_dropdown"><li ><a href="faq.html">Product 1</a></li></ul></li><li><a href="contact.html">Contact</a></li><li id="sup_dropfrom"><a href="support.html">Support&#9660;</a><ul id="sup_dropdown"><li><a href="faq.html">FAQ</a></li><li><a href="manuals.html">Manuals</a></li><li><a href="software.html">Software</a></li></ul></li><li id="about_dropfrom"><a href="about.html">About&#9660;</a><ul id="about_dropdown"><li ><a href="employees.html">Employees</a></li></ul></li></li></ul></div></nav>'
+	var navbar = '<nav class="fixed-nav-bar"> <div id="menu" class="menu"><ul class="menu_items"><li id="logo"><a href="homepage.html"><img src="Images/logo.jpg" alt="logo"></a></li><li><a href="homepage.html">Home</a></li><li><a href="products.html">Products</a></li><li><a href="contact.html">Contact</a></li><li id="sup_dropfrom"><a href="support.html">Support&#9660;</a><ul id="sup_dropdown"><li><a href="faq.html">FAQ</a></li><li><a href="manuals.html">Manuals</a></li><li><a href="software.html">Software</a></li></ul></li><li id="about_dropfrom"><a href="about.html">About&#9660;</a><ul id="about_dropdown"><li ><a href="employees.html">Employees</a></li></ul></li></li></ul></div></nav>'
 	document.getElementById("navbarslot").innerHTML = navbar;
 }
 
@@ -23,9 +20,6 @@ function setup()
  		document.getElementById("sup_dropdown").style.display="table";
  	}
  	else if (n == 2){
- 		document.getElementById("pro_dropdown").style.display="table";
- 	}
- 	else if (n == 3){
  		document.getElementById("about_dropdown").style.display="table";
  	}
 
@@ -36,9 +30,6 @@ function hideMenu(n){
  		document.getElementById("sup_dropdown").style.display="none";
  	}
  	else if (n == 2){
- 		document.getElementById("pro_dropdown").style.display="none";
- 	}
- 	else if (n == 3){
  		document.getElementById("about_dropdown").style.display="none";
  	}
 
