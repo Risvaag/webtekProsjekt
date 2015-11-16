31b562cf8d15c9592553ca363295064fa94bab83
Andreas Risvaag <andreasr7@hotmail.no>
2015-11-16 10:13:37 +0100

    fixed the purchase buttons

--- javascript/script.js
+++ javascript/script.js
@@ -35,9 +35,9 @@ function reset1()
 	else{
 		document.getElementById("Terminator").innerHTML = "";
 	}
-	document.getElementById("button1").innerHTML = "<button id=TerminatorButton>show more</button><button class='purchaseButton'>Purchase</button>";
+	document.getElementById("button1").innerHTML = "<button id=TerminatorButton>Show more</button><button id='TerminatorPurchase' class='purchaseButton'>purchase</button>";
 	document.getElementById("TerminatorButton").addEventListener("click", TerminatorShowMore);
-	addEventListeners();
+	document.getElementById("TerminatorPurchase").addEventListener("click", purchase);
 }
 
 function reset2()
@@ -49,7 +49,7 @@ function reset2()
 	else{
 		document.getElementById("Vacutron").innerHTML = "";
 	}
-	document.getElementById("button2").innerHTML = "<button id=VacutronButton>show more</button><button class='purchaseButton'>Purchase</button>";
+	document.getElementById("button2").innerHTML = "<button id=VacutronButton>Show more</button><button id='VacutronPurchase' class='purchaseButton'>Purchase</button>";
 	document.getElementById("VacutronButton").addEventListener("click", VacutronShowMore);
 	addEventListeners();
 }
@@ -63,14 +63,14 @@ function reset3()
 	else{
 		document.getElementById("Suckmaster").innerHTML = "";
 	}
-	document.getElementById("button3").innerHTML = "<button id=SuckmasterButton>show more</button><button class='purchaseButton'>Purchase</button>";
+	document.getElementById("button3").innerHTML = "<button id=SuckmasterButton>Show more</button><button id='SuckmasterPurchase' class='purchaseButton'>Purchase</button>";
 	document.getElementById("SuckmasterButton").addEventListener("click", SuckmasterShowMore);
 	addEventListeners();
 }
 
 function TerminatorShowMore()
 {
-	document.getElementById("button1").innerHTML = "<button id=TerminatorButton>Show less</button><button class='purchaseButton'>Purchase</button>";
+	document.getElementById("button1").innerHTML = "<button id=TerminatorButton>Show less</button><button id='TerminatorPurchase' class='purchaseButton'>purchase</button>";
 	var input = "<img class='productimg' src='images/r2d2.jpg' alt='The terminator in all it&#39;s glory'><p>"+" The terminator uses state of the art technology to always find it's way through your home with no issues. It's highly advanced sensors makes it able to adapt to most any challenge on the fly and it's wide arange of actuators ensures it's equiped to deal with anything it may encounter on it's quest to terminate <strike>Sarah Connor</strike> dirt in your home</p>"
 	document.getElementById("Terminator").innerHTML += input;
 	document.getElementById("TerminatorButton").addEventListener("click", reset1);
@@ -80,7 +80,7 @@ function TerminatorShowMore()
 
 function VacutronShowMore()
 {
-	document.getElementById("button2").innerHTML = "<button id=VacutronButton>Show less</button><button class='purchaseButton'>Purchase</button>";
+	document.getElementById("button2").innerHTML = "<button id=VacutronButton>Show less</button><button id='VacutronPurchase' class='purchaseButton'>purchase</button>";
 	document.getElementById("Vacutron").innerHTML += "<img class='productimg' src='images/robotcleaner.jpg' alt ='The amazing vacutron'><p>While cheaper and less advanced than the terminator the vacutron remains an impressive accomplishment within the field of artificial inteligence. It's sensors and actuators are easily capable of identifying and overcoming most obstacles it may encounter and it's complex dust detection algorithm ensures it will always clean out every last spec of dust in your house.</p>";
 	document.getElementById("VacutronButton").addEventListener("click", reset2);
 	addEventListeners();
@@ -88,7 +88,7 @@ function VacutronShowMore()
 
 function SuckmasterShowMore()
 {
-	document.getElementById("button3").innerHTML = "<button id=SuckmasterButton>Show less</button><button class='purchaseButton'>Purchase</button>";
+	document.getElementById("button3").innerHTML = "<button id=SuckmasterButton>Show less</button><button id='SuckmasterPurchase' class='purchaseButton'>purchase</button>";
 	document.getElementById("Suckmaster").innerHTML += "<img class='productimg' src='images/shittyVacuum.jpg' alt='The suckmaster'> <p>Pretty much as effective as straping a vacuumcleaner to your dog and hoping it will clean your home but you get what you pay for. While it has issues dealing with chairs, walls and other obstacles it can on ocation sufice if the area you seek to clean is flat and contains nothing at all at floor height.</p>";
 	document.getElementById("SuckmasterButton").addEventListener("click", reset3);
 	addEventListeners();
